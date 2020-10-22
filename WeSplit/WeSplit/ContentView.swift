e26431c9dbe74f57c6679cac50b3d93e71450f90
Denny Mathew <devdenmathew@gmail.com>
2020-10-22 22:45:16 +0530

    Initial commit.

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Denny Mathew on 22/10/20.
+//  Copyright © 2020 Denmathew. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
